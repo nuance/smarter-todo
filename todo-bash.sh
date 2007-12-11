8184210356b8a055394eb9a6a45cc334578b998a
matt <matt@9c6eb874-290c-0410-81ff-d1bceda514d3>
2007-12-11 06:04:36 +0000

    don't wipe out state
    git-svn-id: svn+ssh://mhjones.org/srv/svn-matt/todo-drop@535 9c6eb874-290c-0410-81ff-d1bceda514d3

--- todo-bash.sh
+++ todo-bash.sh
@@ -3,15 +3,19 @@ DROP="/home/matt/todo-drop"
 alias t="$DROP/todo.py"
 
 function tw {
-    $DROP/todo.py x @work @okr p:tests
+		CURRENT=`$DROP/todo.py xl`
+    $DROP/todo.py xa @work @okr p:tests
     $DROP/todo.py $*
-    $DROP/todo.py x
+    $DROP/todo.py xr
+		$DROP/todo.py x $CURRENT
 }
 
 function tc {
-    $DROP/todo.py x @class @soda p:class p:e190 p:cs188 p:chem1a p:cs281a p:research
+		CURRENT=`$DROP/todo.py xl`
+    $DROP/todo.py xa @class @soda p:class p:e190 p:cs188 p:chem1a p:cs281a p:research
     $DROP/todo.py $*
     $DROP/todo.py x
+		$DROP/todo.py x $CURRENT
 }
 
 alias b="$DROP/todo.py b"

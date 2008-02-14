17b4b513518b277f56978942920fed1913cec4d0
matt <matt@9c6eb874-290c-0410-81ff-d1bceda514d3>
2008-02-14 22:26:53 +0000

    fix home
    git-svn-id: svn+ssh://mhjones.org/srv/svn-matt/todo-drop@631 9c6eb874-290c-0410-81ff-d1bceda514d3

--- todo-bash.sh
+++ todo-bash.sh
@@ -1,21 +1,21 @@
-DROP="/home/matt/todo-drop"
+DROP="$HOME/todo-drop"
 
 alias t="$DROP/todo.py"
 
 function tw {
-		CURRENT=`$DROP/todo.py xl`
+    CURRENT=`$DROP/todo.py xl`
     $DROP/todo.py xa @work @okr p:tests
     $DROP/todo.py $*
     $DROP/todo.py xr
-		$DROP/todo.py x $CURRENT
+    $DROP/todo.py x $CURRENT
 }
 
 function tc {
-		CURRENT=`$DROP/todo.py xl`
+    CURRENT=`$DROP/todo.py xl`
     $DROP/todo.py xa @class @soda @cory p:class p:nlp p:pml p:espm p:cs194 p:research
     $DROP/todo.py $*
     $DROP/todo.py x
-		$DROP/todo.py x $CURRENT
+    $DROP/todo.py x $CURRENT
 }
 
 alias b="$DROP/todo.py b"
